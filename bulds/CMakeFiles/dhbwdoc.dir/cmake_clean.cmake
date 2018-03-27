b3ae7de925ee2e31e22c6d836a97236946fa9e69
Benedikt Schultes <b.schultes@gmx.de>
2018-03-27 14:28:36 +0200

    Deleted bulds folder

--- bulds/CMakeFiles/dhbwdoc.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dhbwdoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dhbwdoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

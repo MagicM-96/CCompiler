2e10c26839195347941a6ae427e0abad8720eb8c
EMBaudert <abi2015-merlin@gmx.de>
2018-05-08 10:09:26 +0000

    finallize tempCode

--- sucessTest1.sh
+++ sucessTest1.sh
@@ -4,5 +4,14 @@ rm -r build && mkdir build && cd build && cmake .. && make
 #Feel free to change the tests you want to look at
 #This file is going to be on the .gitIgnore list
 ./dhbwcc ../testing/tests/success/t01.c
-./dhbwcc ../testing/tests/success/t11.c
+./dhbwcc ../testing/tests/success/t02.c
+./dhbwcc ../testing/tests/success/t03.c
+./dhbwcc ../testing/tests/success/t04.c
+./dhbwcc ../testing/tests/success/t05.c
+./dhbwcc ../testing/tests/success/t06.c
+./dhbwcc ../testing/tests/success/t07.c
+./dhbwcc ../testing/tests/success/t08.c
 ./dhbwcc ../testing/tests/success/t09.c
+./dhbwcc ../testing/tests/success/t10.c
+./dhbwcc ../testing/tests/success/t11.c
+./dhbwcc ../testing/tests/success/t12.c
